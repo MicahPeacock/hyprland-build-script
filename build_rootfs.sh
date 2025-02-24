947edcde79866b5891d36cdc5d06a80d3411e1bb
Micah Peacock <35236977+MicahPeacock@users.noreply.github.com>
2025-02-24 16:42:02 -0700

    Create build_rootfs.sh

---
+++ build_rootfs.sh
@@ -0,0 +1,115 @@
+packages=(
+	# Base Linux
+	base
+  	base-devel
+
+	# Linux kernel
+	amd-ucode
+	linux
+ 	linux-firmware
+	linux-headers
+  	
+	# OSTree dependencies
+	efibootmgr
+	grub
+	ostree
+	which
+
+  	# Desktop dependencies
+   	btrfs-progs
+    	htop
+     	iwd
+  	nano
+   	openssh
+    	smartmontools
+     	uwsm
+	vim
+	wget
+  	wireless_tools
+	wpa_supplicant
+ 	xdg-user-dirs
+	xdg-utils
+
+  	# Hyprland dependencies
+   	dolphin
+   	dunst
+    	grim
+  	hyprland
+	kitty
+ 	polkit-kde-agent
+ 	qt5-wayland
+	qt6-wayland
+ 	slurp
+	wofi
+	xdg-desktop-portal-hyprland
+
+  	# Graphics drivers
+  	libva-mesa-driver
+  	mesa
+  	vulkan-radeon
+  	xf86-video-amdgpu
+  	xf86-video-ati
+  	xorg-xinit
+  	xorg-server
+
+  	# Greeter
+  	sddm
+
+  	# Audio Config
+   	gst-plugin-pipewire
+    	libpulse
+  	pipewire
+	pipewire-alsa
+	pipewire-jack
+	pipewire-pulse
+	wireplumber
+
+  	# Network Config
+  	networkmanager
+  	network-manager-applet
+)
+aur_packages=(
+	yay-bin
+)
+
+prepare() {
+	# We need the ostree hook.
+	install -d "$rootfs/etc"
+	install -m 0644 mkinitcpio.conf "$rootfs/etc/"
+}
+
+# This is needed to be able to install AUR packages.
+post_install_early() {
+	echo 'Server = https://geo.mirror.pkgbuild.com/$repo/os/$arch' > /etc/pacman.d/mirrorlist
+	pacman-key --init
+	pacman-key --populate
+}
+
+post_install() {
+	mkdir -p /boot/efi
+
+	# Timezone
+	ln -sf /usr/share/zoneinfo/Canada/Mountain /etc/localtime
+
+	# Locale
+	sed -i 's/^#\(en_US.UTF-8 UTF-8\)/\1/' /etc/locale.gen
+	locale-gen
+
+	# User
+	if ! getent passwd micahpeacock > /dev/null; then
+		passwd --lock root
+		
+		useradd -m -G wheel,audio,video,input,storage,power -s /bin/bash -c "Micah Peacock" micahpeacock
+		passwd micahpeacock
+
+  		mkdir -p /home/micahpeacock
+    		chown micahpeacock:micahpeacock /home/micahpeacock
+      		chmod 755 /home/micahpeacock
+	fi
+
+	# Enable services
+	systemctl enable systemd-timesyncd.service
+  	systemctl enable NetworkManager.service
+	systemctl enable systemd-resolved.service
+  	systemctl enable sddm.service
+}
